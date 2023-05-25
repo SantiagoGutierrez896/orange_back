1124b9062f2a7f1b6a89c434f39767d0099d13eb
Santiago Gutierrez Serna <125933900+SantiagoGutierrez896@users.noreply.github.com>
2023-05-25 12:11:17 -0500

    Minga 09 (#4)
    * Actualizando todo el trabajo las carpetas models, data y packagajson
    
    * se agrega tarea minga-09

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 import express from 'express';
-import 'dotenv/config.js'
+import "dotenv/config.js"
 import './config/database.js'
+import indexRouter from './router/index.js'
+import cors from 'cors'
+import morgan from 'morgan'
 
-const server = express(); // crear servidor
-const PORT = process.env.PORT || 8080; // establecer PORT
-const ready = ()=> console.log('server ready on port: ' + PORT)
+const server= express(); //crear servidor
+const PORT = process.env.PORT || 8080; //definir el puerto
+const ready = () => console.log('ready on '+ PORT);
+console.log(process.env.NODE_ENV);
 
 //midelwares
-server.use(express.json())
-server.use(express.urlencoded({ extended: false }))
+server.use(express.json()) //permite entradas y tambien trabajar con formato json
+server.use(express.urlencoded({ extended: true })) //permite capturar consultas complejas
+server.use(cors())       //para permitir orígenes cruzados (front/back)
+server.use(morgan('dev'))  ////para registrar peticiones HTTP
+
+// router
+server.use('/api',indexRouter)
 
 //router + server
-server.listen(PORT,ready);
+server.listen(PORT, ready);
 
-console.log(process.env.NODE_ENV);
